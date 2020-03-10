48180371986f0c83666e51ceaeabe5d1760f4496
David Yepes <davidyepesb@gmail.com>
2020-03-10 10:43:55 -0500

    Se crea  el proyecto m,aven de spring

---
+++ src/main/java/co/com/megacode/MegacodeApplication.java
@@ -0,0 +1,13 @@
+package co.com.megacode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegacodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegacodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/megacode/MegacodeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.megacode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegacodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
