37cd3538c667151de29e69eb3e22d4a52b7a3c66
David Yepes <davidyepesb@gmail.com>
2020-03-12 15:07:59 -0500

    Cors origins toca quitarlo

--- src/main/java/co/com/megacode/controller/LabelController.java
+++ src/main/java/co/com/megacode/controller/LabelController.java
@@ -3,15 +3,13 @@ package co.com.megacode.controller;
 import co.com.megacode.entity.LabelEntity;
 import co.com.megacode.service.LabelService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/labels")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class LabelController {
 
     @Autowired
@@ -20,6 +18,7 @@ public class LabelController {
     @RequestMapping(value = "/app",method = RequestMethod.GET)
     public List<LabelEntity> getAllLabelsWithApp(@RequestParam(name = "app")String app){
         List labels = labelService.getAllLabelsApp(app);
+        System.out.println("csdsadsad");
         return labels;
     }
 
